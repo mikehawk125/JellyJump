267085abd9bf951ef7c1f2e0cfddc3315950c2eb
Leonarda Milevoj <130672137+lmilevoj@users.noreply.github.com>
2023-09-18 17:01:21 +0200

    Customize player name (almost done)

---
+++ Assets/Scripts/SavePlayerNameScript.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class SavePlayerNameScript : MonoBehaviour
+{
+    public string playerName;
+    public GameObject inputField;
+
+
+    public void StoreName()
+    {
+        // playerName = inputField.text;
+        // Debug.Log(playerName);
+    }
+}
