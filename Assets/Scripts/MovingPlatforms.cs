c3926c38b93ae9a06628863f8be5763611f9865d
marijanciceranUNIPU <115954072+marijanciceranUNIPU@users.noreply.github.com>
2023-09-25 10:48:41 +0200

    Fix platformi i level 7

--- Assets/Scripts/MovingPlatforms.cs
+++ Assets/Scripts/MovingPlatforms.cs
@@ -5,11 +5,12 @@ public class MovingPlatforms : MonoBehaviour
     public Transform platform;
     public Transform startPoint;
     public Transform endPoint;
+    public AnimationCurve timeCurve;
     public float speed = 2.0f;
 
     void Update()
     {
-        float t = Mathf.PingPong(Time.time * speed, 1.0f);
+        float t = timeCurve.Evaluate(Mathf.PingPong(Time.time * speed, 1.0f));
         platform.position = Vector3.Lerp(startPoint.position, endPoint.position, t);
     }
 }
